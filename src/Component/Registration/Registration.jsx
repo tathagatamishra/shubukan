5433881fc16368e38683c062fd73e610406872a9
tathagatamishra <tathagatamishra007@gmail.com>
2024-10-24 10:04:09 +0530

    backend

--- src/Component/Registration/Registration.jsx
+++ src/Component/Registration/Registration.jsx
@@ -25,7 +25,7 @@ export default function Registration() {
     console.log(formData);
     
     try {
-      const response = await fetch('http://localhost:1234/registration', {
+      const response = await fetch('https://shubukan-backend.vercel.app/registration', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
